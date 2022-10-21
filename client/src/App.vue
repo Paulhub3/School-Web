be43947a7066dcebc2bf093e352e8a02e1e4a399
Paulhub3 <somtopaul235@gmail.com>
2022-10-21 11:53:55 +0100

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Navbar from "./components/layouts/Navbar.vue";
+import Footer from "./components/layouts/Footer.vue";
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div>
+    <!-- Navbar Component -->
+    <header>
+      <Navbar />
+    </header>
+
+    <!-- For Viewing Router -->
+    <RouterView />
+
+    <!-- Footer Component -->
+    <div>
+      <Footer />
+    </div>
+  </div>
+</template>
